5a03e79936e63c57692b34332d6cff4b3b78c5ee
Julien <julienconte@isep.fr>
2018-01-11 11:41:02 +0100

    Comit test rest
    I comit the test concerning the rest java class + spring's main and some ressources for rest configuration + index.jsp + some configuration (like gradle)

---
+++ src/test/java/com/resources/CrawlerRestSpringControllerTest.java
@@ -0,0 +1,79 @@
+package com.resources;
+
+import com.crawler.Crawler;
+import com.resources.CrawlerRestSpringController;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+
+public class CrawlerRestSpringControllerTest {
+    /**
+     * Mock the crawler
+     */
+    @Mock
+    private Crawler crawlerMock;
+
+    /**
+     * Inject the mock
+     */
+    @InjectMocks
+    private CrawlerRestSpringController controller;
+
+
+    /**
+     * Setup the crawler returns  for the futur tests
+     */
+    @Before
+    public void initMocks() {
+
+        MockitoAnnotations.initMocks(this);
+        when(crawlerMock.generalCrawler("x")).thenReturn("Légende");
+        when(crawlerMock.information()).thenReturn("de");
+        when(crawlerMock.findFirstMovieBookMusic()).thenReturn("Korra");
+    }
+
+    /**
+     * Test of searchSpecificItem
+     * We need to test a string return
+     */
+    @Test
+    public void testSpecificItem() {
+        assertTrue(controller.searchSpecificItem("x").equals("Légende"));
+    }
+
+    /**
+     * Test of searchSpecificItem
+     * This is a stub method
+     * We replace the used functions and put the attended result
+     * We need to test a string return
+     */
+    @Test
+    //Stub
+    public void testInformation()  {
+
+        controller = new CrawlerRestSpringController();
+
+        controller.setCrawler(new Crawler() {
+            public String information() {
+                return "de";
+            }
+        });
+        assertTrue(controller.information().equals("de"));
+    }
+
+
+    /**
+     * Test of findFirstMovieBookMusic
+     * We need to test a string return
+     */
+    @Test
+    public void testFindFirstMovieBookMusic() {
+        assertTrue(controller.itemByCategory().equals("Korra"));
+    }
+}
