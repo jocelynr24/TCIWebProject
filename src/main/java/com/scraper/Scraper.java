4afda20ec2dce39193e84cd72b91c79c59b0734e
Jbleblanc <jb.leblanc9@gmail.com>
2018-01-11 12:38:19 +0100

    Test
    Writting of the first test class : ScraperTest
    Writting of the class : Scraper

--- src/main/java/com/scraper/Scraper.java
+++ src/main/java/com/scraper/Scraper.java
@@ -1,4 +1,16 @@
 package com.scraper;
 
 public class Scraper {
+
+    public Scraper(){};
+
+
+    /**
+     * @param url
+     * @return a string which contains the json
+     */
+
+    public String scrap(String url){
+        return null;
+    }
 }
