27b0fb5ee0ce32d3b906477cab1974552adfb367
Julien <julienconte@isep.fr>
2018-01-14 00:53:45 +0100

    some correction and project done

--- src/main/java/com/resources/CrawlerRestSpringController.java
+++ src/main/java/com/resources/CrawlerRestSpringController.java
@@ -14,7 +14,7 @@ public class CrawlerRestSpringController {
      * Our "Database" => Crawler
      */
     Crawler crawler = new Crawler();
-
+    String url="https://i398507.hera.fhict.nl/tci/";
     /**
      * For the stub test, we need to have a setter for the crawler
      * @param crawler
@@ -25,14 +25,14 @@ public class CrawlerRestSpringController {
 
     /**
      *
-	 * @param URL (link to crawl the website)
+	 * 
      * @param item (Title of a music/book/movie)
      * @Method Get
      * @return String/json result (movie/book/music)
      */
-    @RequestMapping(value="/searchspecificitem/{URL}/{item}", method = RequestMethod.GET, produces = "application/json")
-    public String searchSpecificItem(@PathVariable String URL, @PathVariable String item)  {
-        return  crawler.getSpecificItem(URL.toLowerCase(), item);
+    @RequestMapping(value="/searchspecificitem/{item}", method = RequestMethod.GET, produces = "application/json")
+    public String searchSpecificItem( @PathVariable String item)  {
+        return  crawler.getSpecificItem(url.toLowerCase(), item);
     }
 
 //    /**
@@ -47,14 +47,14 @@ public class CrawlerRestSpringController {
 //    }
 
     /**
-	 * @param URL (link to crawl the website)
+	 *
      * @Method Get
      * @return String/json result containing data of the first movie, the first book and the first music met
      */
-    @RequestMapping(value="/itembycategory/{URL}", method = RequestMethod.GET, produces = "application/json")
-    public String itemByCategory(@PathVariable String URL){
+    @RequestMapping(value="/itembycategory", method = RequestMethod.GET, produces = "application/json")
+    public String itemByCategory(){
 
-        return crawler.getAllItems(URL.toLowerCase());
+        return crawler.getAllItems(url.toLowerCase());
 
     }
 
