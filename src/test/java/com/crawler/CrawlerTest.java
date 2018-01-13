10da57758d48bc7772565abdf9d6692f9d809e93
Jocelyn <jocelyn.routin@outlook.fr>
2018-01-13 18:57:38 +0100

    Add getSpecificItem() tests
    Add getSpecificItem() method tests of the crawler class

--- src/test/java/com/crawler/CrawlerTest.java
+++ src/test/java/com/crawler/CrawlerTest.java
@@ -55,4 +55,55 @@ public class CrawlerTest {
         // Act (Giving a wrong URL to the basicWebCrawler, it should throw an exception)
         crawler.webCrawler("Worng URL");
     }
+
+    /**
+     * The getSpecificItemReturnsAString() method should always return a string result.
+     * We are testing here if this statement is true.
+     */
+    @Test
+    public void getSpecificItemReturnsAString(){
+        // Act (we give a URL and a name to find something)
+        String specificItem = crawler.getSpecificItem("https://i398507.hera.fhict.nl/tci/", "Office Space");
+        // Assert (that the previously returned item is a string)
+        Assert.assertThat(specificItem, instanceOf(String.class));
+    }
+
+    /**
+     * The getSpecificItemReturnsAString() method should return a JSON string when we give a URL and a name.
+     * We are testing here if the JSON returned by the crawler is the same JSON file that we expect (for example, with "Office Space").
+     */
+    @Test
+    public void getSpecificItemReturnsValueForOfficeSpace(){
+        // Arrange (we define the expected JSON string)
+        String expectedJSON = "{\"result\":{\"director\":\"Mike Judge\",\"format\":\"Blu-ray\",\"genre\":\"Comedy\",\"id\":202,\"name\":\"Office Space\",\"stars\":[\"Ron Livingston\",\" Jennifer Aniston\",\" David Herman\",\" Ajay Naidu\",\" Diedrich Bader\",\" Stephen Root\"],\"writers\":[\"William Goldman\"],\"year\":1999}}";
+        // Act
+        // We crawl the website to find the item
+        String specificItem = crawler.getSpecificItem("https://i398507.hera.fhict.nl/tci/", "Office Space");
+        // We remove the time value on the JSON which will be always different
+        specificItem = specificItem.replaceAll(",\"time\":\\d{1,20}", "");
+        // Assert (that the expected JSON is the same than the returned JSON)
+        Assert.assertEquals(expectedJSON, specificItem);
+    }
+
+    /**
+     * The getSpecificItemReturnsAString() method should throw an exception if we give a wrong URL format.
+     * We are testing here if we get this exception.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void getSpecificItemHasAnInvalidURL(){
+        // Act (we give a wrong URL, it should throw an exception)
+        crawler.getSpecificItem("Invalid URL", "Office Space");
+    }
+
+    /**
+     * The getSpecificItemReturnsAString() method should return an empty string if we give a name which is not on the website.
+     * We are testing here if we get an empty result
+     */
+    @Test
+    public void getSpecificItemFoundNothing(){
+        // Act (we crawl the website to find our not existing element)
+        String specificItem = crawler.getSpecificItem("https://i398507.hera.fhict.nl/tci/", "This item does not exist");
+        // Assert (that the result is an empty string)
+        Assert.assertEquals("", specificItem);
+    }
 }
