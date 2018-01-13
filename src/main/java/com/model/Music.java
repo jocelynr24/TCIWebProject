f12a064766f1b170ac07b97f6c5390d5a08da302
Jocelyn <jocelyn.routin@outlook.fr>
2018-01-13 17:36:25 +0100

    Renamed model
    Renamed model classes for Jenkins coverage tool (Jacoco)

--- src/main/java/com/model/Music.java
+++ src/main/java/com/model/Music.java
@@ -1,28 +1,24 @@
 package com.model;
 
 /**
- * Model class of the movie objects we can find on the website.
+ * Model class of the music objects we can find on the website.
  */
 
-public class MovieModel {
+public class Music {
     private int id;
     private String name;
     private String genre;
     private String format;
     private int year;
-    private String director;
-    private String [] writers;
-    private String [] stars;
+    private String artist;
 
-    public MovieModel(int id, String name, String genre, String format, int year, String director, String[] writers, String[] stars) {
+    public Music(int id, String name, String genre, String format, int year, String artist) {
         this.id = id;
         this.name = name;
         this.genre = genre;
         this.format = format;
         this.year = year;
-        this.director = director;
-        this.writers = writers;
-        this.stars = stars;
+        this.artist = artist;
     }
 
     public int getId() {
@@ -65,27 +61,11 @@ public class MovieModel {
         this.year = year;
     }
 
-    public String getDirector() {
-        return director;
+    public String getArtist() {
+        return artist;
     }
 
-    public void setDirector(String director) {
-        this.director = director;
-    }
-
-    public String[] getWriters() {
-        return writers;
-    }
-
-    public void setWriters(String[] writers) {
-        this.writers = writers;
-    }
-
-    public String[] getStars() {
-        return stars;
-    }
-
-    public void setStars(String[] stars) {
-        this.stars = stars;
+    public void setArtist(String artist) {
+        this.artist = artist;
     }
 }
