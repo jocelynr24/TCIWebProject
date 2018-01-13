f7e7773f9b8488d971a710f6e663262b3ef3002e
Jocelyn <jocelyn.routin@outlook.fr>
2018-01-13 19:01:59 +0100

    Add getSpecificItem() method
    Add getSpecificItem() in the crawler class, plus the needed models

---
+++ src/main/java/com/model/SpecificItem.java
@@ -0,0 +1,28 @@
+package com.model;
+
+public class SpecificItem {
+    //private int id;
+    private long time;
+    private Object result;
+
+    public SpecificItem(long time, Object result) {
+        this.time = time;
+        this.result = result;
+    }
+
+    public Object getResult() {
+        return result;
+    }
+
+    public void setResult(Object result) {
+        this.result = result;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+}
